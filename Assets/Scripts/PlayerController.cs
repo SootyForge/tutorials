a7a715c7a8cc22f980867175cc5c8cac2a1c25a6
SootyForge <42379545+SootyForge@users.noreply.github.com>
2018-09-11 15:44:20 +1000

    End Tutorial Week 8

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+/*
+ * The PlayerController handles player movement & rotation logic
+ */
+public class PlayerController : MonoBehaviour
+{
+    public NavMeshAgent agent; // reference to NavMeshAgent component
+
+    public Vector3 targetPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the targetPos is valid (is not zero)
+        if(targetPos.magnitude > 0)
+        {
+            // Follow the target!
+            agent.SetDestination(targetPos);
+        }
+    }
+
+    // Call this to tell agent where to go!
+    public void SetTarget(Vector3 target)
+    {
+        // Set the new target
+        targetPos = target;
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * The PlayerInput is in charge of recieving input
+ * from the user and ineracting with PlayerController
+ */
+public class PlayerInput : MonoBehaviour
+{
+    public PlayerController controller;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if Mouse Button is down
+        // 0 = Left
+        // 1 = Right
+        // 2 = Middle
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Get ray from camera
+            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            // Raycast out to hit something
+            if (Physics.Raycast(camRay, out hit, 1000f))
+            {
+                // Update the target with the point we hit
+                controller.SetTarget(hit.point);
+            }
+        }
+    }
+}
